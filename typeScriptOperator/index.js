509711d7ec7753cbf1c2360043964c2c20005ad0
jatinKamboj <kumar.jatin873@gmail.com>
2022-12-22 20:38:08 +0530

    Basic Operators

---
+++ typeScriptOperator/index.js
@@ -0,0 +1,61 @@
+// 1) Arithmetic Operators
+// addition
+var a1 = 20;
+var b1 = 30;
+var c1 = a1 + b1;
+console.log('Addition => ', c1);
+// subtraction
+var a2 = 30;
+var b2 = 20;
+var c2 = a2 - b2;
+console.log('Subtraction => ', c2);
+// Multiplication
+var a3 = 30;
+var b3 = 20;
+var c3 = a3 * b3;
+console.log('Multiplication => ', c3);
+// Division
+var a4 = 100;
+var b4 = 20;
+var c4 = a4 / b4;
+console.log('Division => ', c4);
+// Modulus
+var a5 = 95;
+var b5 = 20;
+var c5 = a5 % b5;
+console.log('Modulus => ', c5);
+// Increment
+var a6 = 55;
+a6++;
+console.log('Increment => ', a6);
+// Decrement
+var a7 = 55;
+a7--;
+console.log('Decrement => ', a7);
+// 2) Comparison or Relation Operators
+// is equal to ==
+var a8 = 10;
+var b8 = 20;
+console.log('a8 == b8 => ', a8 == b8);
+console.log('a8 == 10 => ', a8 == 10);
+// identical (===)
+console.log('a8 === b8 => ', a8 === b8);
+console.log('a8 === 10 => ', a8 === 10);
+// Not equal to !=
+console.log('a8 != b8 => ', a8 != b8);
+console.log('a8 != 10 => ', a8 != 10);
+// Not Identical !==
+console.log('a8 !== b8 => ', a8 !== b8);
+console.log('a8 !== 10 => ', a8 !== 10);
+// Greater Than
+console.log('a8 > b8 => ', a8 > b8);
+console.log('a8 > 30 => ', a8 > 30);
+// Greater Than Equal To
+console.log('a8 >= b8 => ', a8 >= b8);
+console.log('a8 >=30 => ', a8 >= 30);
+// Less Than
+console.log('a8 < b8 => ', a8 < b8);
+console.log('a8 < 10 => ', a8 < 10);
+// Less Than Equal To
+console.log('a8 <= b8 => ', a8 <= b8);
+console.log('a8 <= 10 => ', a8 <= 10);
---
+++ typeScriptOperator/index.ts
@@ -0,0 +1,77 @@
+// 1) Arithmetic Operators
+// addition
+let a1 = 20;
+let b1 = 30;
+let c1 = a1 + b1;
+console.log('Addition => ', c1);
+
+// subtraction
+let a2 = 30;
+let b2 = 20;
+let c2 = a2 - b2;
+console.log('Subtraction => ', c2);
+
+// Multiplication
+let a3 = 30;
+let b3 = 20;
+let c3 = a3 * b3;
+console.log('Multiplication => ', c3);
+
+// Division
+let a4 = 100;
+let b4 = 20;
+let c4 = a4 / b4;
+console.log('Division => ', c4);
+
+// Modulus
+let a5 = 95;
+let b5 = 20;
+let c5 = a5 % b5;
+console.log('Modulus => ', c5);
+
+// Increment
+let a6 = 55;
+a6++;
+console.log('Increment => ', a6);
+
+// Decrement
+let a7 = 55;
+a7--;
+console.log('Decrement => ', a7);
+
+// 2) Comparison or Relation Operators
+// is equal to ==
+let a8 = 10;
+let b8 = 20;
+console.log('a8 == b8 => ', a8 == b8);
+console.log('a8 == 10 => ', a8 == 10);
+
+// identical (===)
+console.log('a8 === b8 => ', a8 ===  b8);
+console.log('a8 === 10 => ', a8 === 10);
+
+// Not equal to !=
+console.log('a8 != b8 => ', a8 != b8);
+console.log('a8 != 10 => ', a8 != 10);
+
+// Not Identical !==
+console.log('a8 !== b8 => ', a8 !== b8);
+console.log('a8 !== 10 => ', a8 !== 10);
+
+// Greater Than
+console.log('a8 > b8 => ', a8 > b8);
+console.log('a8 > 30 => ', a8 > 30);
+
+// Greater Than Equal To
+console.log('a8 >= b8 => ', a8 >= b8);
+console.log('a8 >=30 => ', a8 >=30);
+
+// Less Than
+console.log('a8 < b8 => ', a8 < b8);
+console.log('a8 < 10 => ', a8 < 10);
+
+// Less Than Equal To
+console.log('a8 <= b8 => ', a8 <= b8);
+console.log('a8 <= 10 => ', a8 <= 10);
+
+
